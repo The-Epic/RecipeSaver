9e5da73a7c2bbaefca8a9d819a2a153fe37e919a
The-Epic <EpicEbic@outlook.com>
2023-03-15 05:05:05 +0000

    Support shaped and shapeless recipes

--- src/main/java/me/epic/recipesaver/RecipeSaver.java
+++ src/main/java/me/epic/recipesaver/RecipeSaver.java
@@ -4,9 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.RecipeChoice;
-import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.*;
 
 import java.util.List;
 
@@ -20,9 +18,9 @@ public final class RecipeSaver  {
      *
      * @param section to load recipe
      * @param namespacedKey NamespacedKey to register the recipe on
-     * @return Shaped recipe from ConfigurationSecton
+     * @return Recipe from ConfigurationSecton
      */
-    public static ShapedRecipe loadRecipe(ConfigurationSection section, NamespacedKey namespacedKey) {
+    public static Recipe loadRecipe(ConfigurationSection section, NamespacedKey namespacedKey) {
         return loadRecipe(section, namespacedKey, null);
     }
 
@@ -33,9 +31,9 @@ public final class RecipeSaver  {
      * @param path to config section
      * @param key NamespacedKey to register the recipe on
      * @param result Result for the recipe
-     * @return ShapedRecipe from FileConfiguration
+     * @return Recipe from FileConfiguration
      */
-    public static ShapedRecipe loadRecipe(FileConfiguration fileConfiguration, String path, NamespacedKey key, ItemStack result) {
+    public static Recipe loadRecipe(FileConfiguration fileConfiguration, String path, NamespacedKey key, ItemStack result) {
         return loadRecipe(fileConfiguration.getConfigurationSection(path), key, result);
     }
 
@@ -45,23 +43,53 @@ public final class RecipeSaver  {
      * @param section to load recipe
      * @param namespacedKey NamespacedKey to register the recipe on
      * @param result Result for the recipe
-     * @return Shaped recipe from ConfigurationSecton
+     * @return Recipe from ConfigurationSecton
      */
-    public static ShapedRecipe loadRecipe(ConfigurationSection section, NamespacedKey namespacedKey, ItemStack result) {
-        ShapedRecipe recipe = new ShapedRecipe(namespacedKey, result == null ? new ItemStack(Material.matchMaterial(section.getString("result"))) : result);
-        ConfigurationSection items = section.getConfigurationSection("items");
-        List<String> shape = section.getStringList("shape");
-        recipe.shape(shape.toArray(String[]::new));
+    public static Recipe loadRecipe(ConfigurationSection section, NamespacedKey namespacedKey, ItemStack result) {
+        switch (section.getString("type")) {
+            case "shaped" -> {
+                ShapedRecipe recipe = new ShapedRecipe(namespacedKey, result == null ? new ItemStack(Material.matchMaterial(section.getString("result"))) : result);
+                ConfigurationSection items = section.getConfigurationSection("items");
+                List<String> shape = section.getStringList("shape");
+                recipe.shape(shape.toArray(String[]::new));
+                addItems(items, recipe);
+
+                return recipe;
+            }
+            case "shapeless" -> {
+                ShapelessRecipe recipe = new ShapelessRecipe(namespacedKey, result == null ? new ItemStack(Material.matchMaterial(section.getString("result"))) : result);
+                ConfigurationSection items = section.getConfigurationSection("items");
+                addItems(items, recipe);
+                return recipe;
+
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Adds items for both recipes
+     *
+     * @param items Configuration section to get the items from
+     * @param recipe to add the ingredients to
+     */
+    private static void addItems(ConfigurationSection items, Recipe recipe) {
         for (String key : items.getKeys(false)) {
             if (items.isList(key)) {
                 RecipeChoice choice = new RecipeChoice.MaterialChoice(items.getStringList(key).stream().map(Material::matchMaterial).toArray(Material[]::new));
-                recipe.setIngredient(key.charAt(0), choice);
+                if (recipe instanceof ShapelessRecipe shapelessRecipe) {
+                    shapelessRecipe.addIngredient(choice);
+                } else {
+                    ((ShapedRecipe) recipe).setIngredient(key.charAt(0), choice);
+                }
                 continue;
             }
-            recipe.setIngredient(key.charAt(0), Material.matchMaterial(items.getString(key)));
+            if (recipe instanceof ShapelessRecipe shapelessRecipe) {
+                shapelessRecipe.addIngredient(Material.matchMaterial(items.getString(key)));
+            } else {
+                ((ShapedRecipe) recipe).setIngredient(key.charAt(0), Material.matchMaterial(items.getString(key)));
+            }
         }
-
-        return recipe;
     }
 
 }
